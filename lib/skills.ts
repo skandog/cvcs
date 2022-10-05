7a50534d23c74cd2da6d0147fdcc69aeaefea6cc
skandog <muumrar@gmail.com>
2022-10-05 11:19:28 +0100

    stubbed a response of skills for backend standin

---
+++ lib/skills.ts
@@ -0,0 +1,9 @@
+type Skill = {
+  name: string;
+  type?: string;
+};
+
+export const dummySkillList: Skill[] = [
+  {name: 'Trail Blazer', type: 'Soft Skills'},
+  {name: 'Typescript', type: 'dev'},
+];
