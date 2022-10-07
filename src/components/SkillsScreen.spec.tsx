3a92a9098c8d662c6b1a3ae259c65f9f7df3c923
skandog <muumrar@gmail.com>
2022-10-07 13:01:20 +0100

    lunch time - having a lot of trouble getting the context tests passing

--- src/components/SkillsScreen.spec.tsx
+++ src/components/SkillsScreen.spec.tsx
@@ -1,4 +1,6 @@
 import {render, screen} from '@testing-library/react';
+import {ReactElement} from 'react';
+import {skillsContext} from '../contexts/contexts';
 import SkillsScreen from './SkillsScreen';
 
 describe('<SkillsList />', () => {
@@ -10,6 +12,39 @@ describe('<SkillsList />', () => {
     it('shows default value of undefined', () => {
       renderComponent();
       expect(screen.queryByText('Trail Blazer')).not.toBeInTheDocument();
+    });
+
+    const customRender = (
+      ui: ReactElement,
+      {providerProps, ...renderOptions},
+    ) => {
+      return render(
+        <skillsContext.Provider value={providerProps}>
+          {ui}
+        </skillsContext.Provider>,
+        renderOptions,
+      );
+    };
+
+    it('skillsContext provides skill values to component', () => {
+      const providerProps = {
+        value: [{id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'}],
+      };
+
+      //     value: [{id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'}],
+      //   };
+
+      //   customRender(<SkillsScreen />, {providerProps});
+
+      render(
+        <>
+          <skillsContext.Provider {...providerProps}>
+            <SkillsScreen />
+          </skillsContext.Provider>
+          ,
+        </>,
+      );
+      expect(screen.getByText('Trail Blazer')).toBeInTheDocument();
     });
   });
 });
