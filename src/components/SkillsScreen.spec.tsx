e3de2fd5789c414a66ab38dc582069c10515fb06
skandog <muumrar@gmail.com>
2022-10-07 14:39:15 +0100

    completed testing of context

--- src/components/SkillsScreen.spec.tsx
+++ src/components/SkillsScreen.spec.tsx
@@ -2,6 +2,7 @@ import {render, screen} from '@testing-library/react';
 import {ReactElement} from 'react';
 import {skillsContext} from '../contexts/contexts';
 import SkillsScreen from './SkillsScreen';
+import App from '../App';
 
 describe('<SkillsList />', () => {
   function renderComponent() {
@@ -27,23 +28,26 @@ describe('<SkillsList />', () => {
     };
 
     it('skillsContext provides skill values to component', () => {
-      const providerProps = {
-        value: [{id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'}],
-      };
-
       //     value: [{id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'}],
       //   };
 
       //   customRender(<SkillsScreen />, {providerProps});
 
+      render(<App />);
+      expect(screen.getByText('Trail Blazer')).toBeInTheDocument();
+    });
+
+    const skills = [
+      {id: 1, name: 'Trail Blazer', count: 2, type: 'Soft Skills'},
+    ];
+    beforeEach(() => {
       render(
-        <>
-          <skillsContext.Provider {...providerProps}>
-            <SkillsScreen />
-          </skillsContext.Provider>
-          ,
-        </>,
+        <skillsContext.Provider value={skills}>
+          <SkillsScreen />
+        </skillsContext.Provider>,
       );
+    });
+    it('testing contwext in jest', () => {
       expect(screen.getByText('Trail Blazer')).toBeInTheDocument();
     });
   });
