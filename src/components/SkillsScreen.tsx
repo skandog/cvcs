b729b6feea9dc2313c8fcb8856070326d92e3fa0
skandog <muumrar@gmail.com>
2022-10-07 10:32:40 +0100

    got e2e test passign with skills being handed via context. need to link up to back end

--- src/components/SkillsScreen.tsx
+++ src/components/SkillsScreen.tsx
@@ -1,10 +1,13 @@
+import {useContext} from 'react';
+import {skillsContext} from '../contexts/contexts';
 import SkillsList from './SkillsList';
 
 export default function SkillsScreen() {
+  const {skills, setSkills} = useContext(skillsContext);
   return (
     <div>
       <h1>skills</h1>
-      <SkillsList loadSkills={() => {}} />
+      <SkillsList loadSkills={() => {}} skills={skills} />
     </div>
   );
 }
