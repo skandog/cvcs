881e4e6c6920269b8b864810c30d117fa2f287f6
skandog <muumrar@gmail.com>
2022-10-11 13:35:28 +0100

    added card component to main page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ import {
 } from '@chakra-ui/react';
 
 function Card(props) {
-  const {product, summary, longLine} = props;
+  const {children} = props;
 
   return (
     <Box
@@ -21,45 +21,22 @@ function Card(props) {
       borderWidth={1}
       margin={2}
     >
-      <AspectRatio ratio={1 / 1}>
+      {/* <AspectRatio ratio={1 / 1}>
         <Image
           maxWidth="200px"
           margin="auto"
           src="https://picsum.photos/id/237/250/250"
           alt="Woman paying for a purchase"
         />
-      </AspectRatio>
+      </AspectRatio> */}
       <Stack
         align={{base: 'center', md: 'stretch'}}
-        textAlign={{base: 'center', md: 'left'}}
-        mt={{base: 4, md: 0}}
+        textAlign={{base: 'left', md: 'left'}}
+        mt={{base: 2, md: 0}}
+        mb={{base: 2, md: 0}}
         ml={{md: 6}}
       >
-        <Text
-          fontWeight="bold"
-          textTransform="uppercase"
-          fontSize="lg"
-          letterSpacing="wide"
-          color="teal.600"
-        >
-          {product}
-        </Text>
-        <Link
-          my={1}
-          display="block"
-          fontSize="md"
-          lineHeight="normal"
-          fontWeight="semibold"
-          href="#"
-        >
-          {summary}
-        </Link>
-        <Text my={2} color="gray.500">
-          {longLine}
-        </Text>
-        <Button maxWidth="100px" my={2}>
-          Click me!
-        </Button>
+        {children}
       </Stack>
     </Box>
   );
--- src/components/SkillsScreen.tsx
+++ src/components/SkillsScreen.tsx
@@ -1,14 +1,18 @@
 import {useContext} from 'react';
 import {skillsContext} from '../contexts/contexts';
 import SkillsList from './SkillsList';
+import Card from './Card';
+import {Heading} from '@chakra-ui/react';
 
 export default function SkillsScreen() {
   const {skills} = useContext(skillsContext);
 
   return (
     <div>
-      <h1>Skills</h1>
-      <SkillsList loadSkills={() => {}} skills={skills} />
+      <Card>
+        <Heading>Skills</Heading>
+        <SkillsList loadSkills={() => {}} skills={skills} />
+      </Card>
     </div>
   );
 }
